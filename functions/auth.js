499cbac04f17d6d5d49f17ee7b09cfee3537f640
Sonaravikumar <sonaravikumar@gmail.com>
2024-12-02 22:27:52 +0530

    updated

--- functions/auth.js
+++ functions/auth.js
@@ -1,42 +1,39 @@
 // middleware/verifyToken.js
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
-const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization']; // Get the Authorization header
-    const token = authHeader && authHeader.split(' ')[1]; // Extract the token from the header
+//Import necessary libraries
+const jwt = require('jsonwebtoken'); 
+require('dotenv').config(); 
 
-    if (token === null) return res.sendStatus(401)
-    try {
-        const tokenResult = jwt.verify(token, process.env.JWT_SECRET);
-        if (tokenResult) {
-            req.email = tokenResult.email; // Attach the email to the request object
-            req.workspace = tokenResult.workspace;
-            next(); // Proceed to the next middleware or route handler
-        } else {
-            return res.status(400).json({ message: 'Invalid token' });
-        }
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid or expired token', error: error.message });
+//Define the middleware function
+const verifyToken = (req, res, next) => {  
+
+  // Get the Authorization header
+  const authHeader = req.headers['authorization']; 
+
+  // Extract the token from the Authorization header
+  const token = authHeader && authHeader.split(' ')[1]; //extract only token from Authorization header   
+
+  // Check if no token is present
+  if (token === null) return res.sendStatus(401);  
+
+  try {
+    // Verify the token with the JWT_SECRET from the environment
+    const tokenResult = jwt.verify(token, process.env.JWT_SECRET);  
+
+    // If token is valid, add user details to the request object
+    if (tokenResult) {
+        req.email = tokenResult.email; // Attach the decoded 'email' from the token to the request object
+        req.workspace = tokenResult.workspace; // Attach the decoded 'workspace' from the token to the request object
+        next(); // Pass control to the next middleware or route handler in the stack
+    } else {
+        return res.status(400).json({ message: 'Invalid token' });  
     }
-  };
+  } catch (error) {
+    //Catch any errors related to the token verification process
+    return res.status(401).json({ message: 'Invalid or expired token', error: error.message });  
+  }
 
-module.exports = verifyToken;
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Export the middleware function
+module.exports = verifyToken; 
