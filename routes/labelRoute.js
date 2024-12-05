f0f2143a5ca0a105fa6fb4af08eba8cfa36d383a
Sonaravikumar <sonaravikumar@gmail.com>
2024-12-05 12:30:18 +0530

    updated

---
+++ routes/labelRoute.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { 
+    addLabel, 
+    listLabels, 
+    deleteLabel, 
+    assignLabel, 
+    encryptDataHandler, 
+    testOperators 
+} = require('../controlllers/labelControllers');
+const authorization = require('../functions/auth');
+
+const router = express.Router();
+
+// Define routes and map them to controller functions
+router.post('/add', authorization, addLabel);
+router.get('/list', authorization, listLabels);
+router.post('/delete', authorization, deleteLabel);
+router.post('/assigns', authorization, assignLabel);
+router.post('/encrypt', encryptDataHandler);
+router.post('/test', authorization, testOperators);
+
+module.exports = router;
